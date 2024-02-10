51affd0930aea611edcff448329c0385e6a53bb1
sonicfind <sonicfind2@gmail.com>
2024-02-09 23:29:16 -0600

    Restructure Loading for GameManager [No compilation]
    + Adds an overload to LoadingManager.Queue that accepts any task that can safely run in parallel. Ex: chart loading.
    
    + Use said overload to allow chart and audio loading to run in parallel.
    
    + Rearrange some chart finalization until after both audio & chart loaded *explicitly* sequentially.

--- Assets/Script/Gameplay/GameManager.Audio.cs
+++ Assets/Script/Gameplay/GameManager.Audio.cs
@@ -4,7 +4,6 @@ using Cysharp.Threading.Tasks;
 using UnityEngine;
 using YARG.Core.Audio;
 using YARG.Core.Chart;
-using YARG.Helpers.Extensions;
 using YARG.Settings;
 
 namespace YARG.Gameplay
@@ -32,7 +31,7 @@ namespace YARG.Gameplay
 
         private int _starPowerActivations = 0;
 
-        private async UniTask LoadAudio()
+        private void LoadAudio()
         {
             // The stem states are initialized in "CreatePlayers"
             _stemStates.Clear();
@@ -42,45 +41,17 @@ namespace YARG.Gameplay
             bool isYargSong = Song.Source.Str.ToLowerInvariant() == "yarg";
             GlobalVariables.AudioManager.Options.UseMinimumStemVolume = isYargSong;
 
-            await UniTask.RunOnThreadPool(() =>
+            try
             {
-                try
-                {
                     Song.LoadAudio(GlobalVariables.AudioManager, GlobalVariables.Instance.SongSpeed);
                     GlobalVariables.AudioManager.SongEnd += OnAudioEnd;
-                }
-                catch (Exception ex)
-                {
-                    _loadState = LoadFailureState.Error;
-                    _loadFailureMessage = "Failed to load audio!";
-                    Debug.LogException(ex, this);
-                }
-            });
-
-            if (_loadState != LoadFailureState.None) return;
-
-            double audioLength = GlobalVariables.AudioManager.AudioLengthD;
-            double chartLength = Chart.GetEndTime();
-            double endTime = Chart.GetEndEvent()?.Time ?? -1;
-
-            // - Chart < Audio < [end] -> Audio
-            // - Audio < Chart < [end] -> Chart
-            // - [end] < Chart < Audio -> Audio
-            // - [end] < Audio < Chart -> Chart
-            if ((endTime >= audioLength && endTime >= chartLength) ||
-                endTime <= audioLength && endTime <= chartLength)
-            {
-                SongLength = Math.Max(audioLength, chartLength);
             }
-            // - Audio < [end] < Chart -> Chart
-            // - Chart < [end] < Audio -> [end]
-            else
+            catch (Exception ex)
             {
-                SongLength = Math.Max(chartLength, endTime);
+                _loadState = LoadFailureState.Error;
+                _loadFailureMessage = "Failed to load audio!";
+                Debug.LogException(ex, this);
             }
-
-            SongLength += SONG_END_DELAY;
-            _songLoaded?.Invoke();
         }
 
         private void StarPowerClap(Beatline beat)
